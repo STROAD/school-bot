89805d02c7c11787b72b8a50daad0f1dda1c0bf9
STROAD <koseok0611@naver.com>
2022-04-03 15:44:50 +0900

    v4.0.0

--- bot_integrated_version/bot_integrated.py
+++ bot_integrated_version/bot_integrated.py
@@ -306,7 +306,7 @@ async def info(ctx):
     embed.add_field(name="School_Bot", value="급식, 버스, 날씨정보 확인가능", inline=False)
     embed.add_field(name="자세한 정보는", value=f"[여기서]({GitHub}) 확인 가능", inline=False)
     embed.add_field(name="\u200B", value="\u200B", inline=False)
-    embed.add_field(name="*버전* : 3.1.0", value=f"[GitHub]({GitHub})", inline=False)
+    embed.add_field(name="*버전* : 4.0.0", value=f"[GitHub]({GitHub})", inline=False)
 
     await ctx.send(embed=embed)
 
