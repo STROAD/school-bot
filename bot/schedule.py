74e622b5fb1fdef7d17f7baa2120d5a2a8ae8b5d
STROAD <koseok0611@naver.com>
2022-06-05 18:03:09 +0900

    Add: 학사일정 API URL

--- bot/schedule.py
+++ bot/schedule.py
@@ -3,3 +3,7 @@ from datetime import datetime
 from requests import get
 from xml.etree.ElementTree import fromstring
 from config import NIES_KEY
+
+
+# 학사일정 API URL
+schedule_url = "https://open.neis.go.kr/hub/SchoolSchedule"
