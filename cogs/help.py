84fb4ad2b83b4b61e5271474654b005e1c750c5f
STROAD <koseok0611@naver.com>
2022-12-11 12:25:21 +0900

    Add: class SelectView
    Discord에서 UI를 보여줄 수 있도록 하는 클래스

--- cogs/help.py
+++ cogs/help.py
@@ -6,3 +6,9 @@ from datetime import datetime
 
 class Select_Help(discord.ui.Select):
     pass
+
+
+class SelectView(discord.ui.View):
+    def __init__(self, *, timeout=180):
+        super().__init__(timeout=timeout)
+        self.add_item(Select_Help())
